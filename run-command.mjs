143dfafca21cb3507c3df25d74fc4e863f9f7b65
CheezzyBoii <169907099+CheezzyBoii@users.noreply.github.com>
2026-09-07 18:39:47 +0000

    add: some comments on the verbose logs

--- run-command.mjs
+++ run-command.mjs
@@ -183,6 +183,7 @@ commands: for (let i = 2; i < process.argv.length; i++)
                   )
                 )
               );
+              // Print the file being compiled
               if (config.verbose) {
                 console.log(`[Build] Compiling file "${file}" from ${base + dir + '/'} to ${targetPath}`);
               }
@@ -192,6 +193,7 @@ commands: for (let i = 2; i < process.argv.length; i++)
           } else if (fileStats.isDirectory()) {
             if (!existsSync(targetPath)) mkdirSync(targetPath);
             compile(file, base + dir + '/', outDir, initialDir, applyRewrites);
+            // Print the directory being compiled
             if (config.verbose){
               console.log(`[Build] Compiling directory "${file}" from ${base + dir + '/'} to ${targetPath}`);
             }
@@ -224,6 +226,7 @@ commands: for (let i = 2; i < process.argv.length; i++)
 
         const prefix = prefixName + '/',
           prefixUrl = new URL('./views/dist-new/' + prefix, import.meta.url);
+          // Print the directory being compiled
           if (config.verbose){
             console.log(`[Build] Compiling "${prefixName}" from ${relSrc} to ${prefixUrl}`);
           }
